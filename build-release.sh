1cf12beaeb48457366234d62b027ee3bfba36f0e
jinxin <jinxin70@gmail.com>
2026-03-08 14:40:56 +0800

    docs: 更新README并修正构建脚本中的别名路径
    在README中添加从源码构建的两种方法说明，使安装指南更清晰。
    同时修正build-release.sh中的alias路径，使其指向正确的Releases目录。

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir "Releases"
 
@@ -14,5 +14,5 @@ CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build  -o ./Releases/m3u8 m3u8-downloa
 # echo "start build windows/amd64 ..."
 # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o ./Releases/m3u8-windows-amd64.exe m3u8-downloader.go
 
-echo 'alias m3u8="~/Documents/m3u8-downloader/m3u8"' >> ~/.zshrc && source ~/.zshrc
+echo 'alias m3u8="~/Documents/m3u8-downloader/Releases/m3u8"' >> ~/.zshrc && source ~/.zshrc
 echo "Congratulations,all build success!!!"
